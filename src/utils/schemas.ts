5eaf8dd09b351ccbe38d4ee6929cbf22f6baaf2a
Pablo Souza <pgeovany14@gmail.com>
2022-09-30 21:24:32 -0300

    feat: implement signUp schema

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,1 +1,10 @@
 import joi from 'joi';
+
+const signUpSchema = joi.object({
+  username: joi.string().required(),
+  email: joi.string().required(),
+  password: joi.string().required(),
+  confirmPassword: joi.string().valid(joi.ref('password')).required(),
+});
+
+export { signUpSchema };
