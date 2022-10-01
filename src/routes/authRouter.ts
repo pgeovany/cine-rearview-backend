034fa0f5384d93d33281fb9c4e3e77487f16a943
Pablo Souza <pgeovany14@gmail.com>
2022-09-30 21:58:48 -0300

    feat: add sign-in route to authRouter

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as authController from '../controllers/authController';
 import validateSchema from '../middlewares/schemaValidator';
-import { signUpSchema } from '../utils/schemas';
+import { signInSchema, signUpSchema } from '../utils/schemas';
 
 const authRouter = Router();
 
@@ -9,6 +9,12 @@ authRouter.post(
   '/sign-up',
   validateSchema(signUpSchema),
   authController.signUp
+);
+
+authRouter.post(
+  '/sign-in',
+  validateSchema(signInSchema),
+  authController.signIn
 );
 
 export default authRouter;
