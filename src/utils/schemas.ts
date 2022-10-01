0b1967db83db74082d494e855e77986d9eafa45c
Pablo Souza <pgeovany14@gmail.com>
2022-09-30 21:56:10 -0300

    feat: add sign-in schema

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -7,4 +7,9 @@ const signUpSchema = joi.object({
   confirmPassword: joi.string().valid(joi.ref('password')).required(),
 });
 
-export { signUpSchema };
+const signInSchema = joi.object({
+  email: joi.string().required(),
+  password: joi.string().required(),
+});
+
+export { signUpSchema, signInSchema };
