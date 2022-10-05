20d652aaf0b86612331b3346770a5d2a33c1ff35
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 12:03:06 -0300

    feat: implement a function to get film details

--- src/services/filmsService.ts
+++ src/services/filmsService.ts
@@ -1,35 +1,28 @@
 import axios from 'axios';
-import FilmData from '../types/films';
+import * as filmUtils from '../utils/filmUtils';
 
-const { API_KEY, SEARCH_URL, IMAGES_URL } = process.env;
+const { API_KEY, SEARCH_URL, FILM_URL } = process.env;
 
 async function getFilmsByName(name: string) {
   const { data } = await axios.get(
     `${SEARCH_URL}api_key=${API_KEY}&query=${name}`
   );
 
-  const films = formatFilms(data.results);
+  const films = filmUtils.formatFilms(data.results);
 
   return films;
 }
 
-function formatFilms(films: FilmData[]) {
-  return films.map((film) => {
-    return {
-      originalId: film.id,
-      title: film.original_title,
-      localTitle: film.title,
-      overview: film.overview,
-      poster: IMAGES_URL + film.poster_path,
-      releaseDate: formatDate(film.release_date),
-    };
-  });
+async function getFilmDetails(id: number) {
+  const { data } = await axios.get(`${FILM_URL}/${id}?api_key=${API_KEY}`);
+
+  const { crew, cast } = await filmUtils.getCrewAndCast(id);
+
+  const directors = filmUtils.getDirectors(crew);
+
+  const film = filmUtils.formatFilm(data);
+
+  return { ...film, directors, crew, cast };
 }
 
-function formatDate(date: string) {
-  // formats date from YYYY-MM-DD format to DD/MM/YYYY
-  const aux = date.split('-');
-  return `${aux[2]}/${aux[1]}/${aux[0]}`;
-}
-
-export { getFilmsByName };
+export { getFilmsByName, getFilmDetails };
