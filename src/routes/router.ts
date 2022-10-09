36daeb16a62e0f417fc9f4911a627180a7c6a8ac
Pablo Souza <pgeovany14@gmail.com>
2022-10-09 17:13:17 -0300

    feat: add watchlistsRouter to main router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import authRouter from './authRouter';
 import filmsRouter from './filmsRouter';
 import userFilmsRouter from './userFilmsRouter';
+import watchlistsRouter from './watchlistsRouter';
 
 const router = Router();
 router.use('/auth', authRouter);
 router.use('/films', filmsRouter);
 router.use(userFilmsRouter);
+router.use(watchlistsRouter);
 
 export default router;
