5480d61b8a27b32ff1949ad25a4fadd56f36c309
Pablo Souza <pgeovany14@gmail.com>
2022-10-09 17:11:17 -0300

    feat: implement watchlist repository

---
+++ src/repositories/watchlistsRepository.ts
@@ -0,0 +1,49 @@
+import prisma from '../database/database';
+import { WatchlistFilms, Films } from '@prisma/client';
+
+export type WatchlistInsertData = Omit<WatchlistFilms, 'id' | 'createdAt'>;
+
+async function create(userId: string, filmId: string) {
+  return await prisma.watchlistFilms.create({
+    data: {
+      userId,
+      filmId,
+    },
+  });
+}
+
+async function remove(userId: string, filmId: string) {
+  return await prisma.watchlistFilms.delete({
+    where: {
+      userId_filmId: {
+        userId,
+        filmId,
+      },
+    },
+  });
+}
+
+async function findUnique(userId: string, filmId: string) {
+  return await prisma.watchlistFilms.findUnique({
+    where: {
+      userId_filmId: {
+        userId,
+        filmId,
+      },
+    },
+  });
+}
+
+async function getUserWatchlist(userId: string) {
+  return await prisma.$queryRaw<Films>`
+    SELECT films.id, "originalId", title, overview, "posterUrl", "releaseDate"
+    from "watchlistFilms"
+    JOIN users
+    ON users.id = "watchlistFilms"."userId"
+    JOIN films
+    ON films.id = "watchlistFilms"."filmId"
+    WHERE users.id = ${userId}
+  `;
+}
+
+export { create, remove, findUnique, getUserWatchlist };
