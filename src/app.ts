c2628c272aded075bb7770a39dd03cb7a16549c3
Pablo Souza <pgeovany14@gmail.com>
2023-05-24 19:46:57 -0300

    feat: implement rate limiter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { rateLimit } from 'express-rate-limit';
 import router from './routes/router';
 import errorHandler from './middlewares/errorHandler';
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 const app = express();
-app.use(cors(), json());
+app.use(cors(), json(), limiter);
 app.use(router);
 app.use(errorHandler);
 
